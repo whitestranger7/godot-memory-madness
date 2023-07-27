dad39e1e5416778927d11f5050727d7eaa226869
Ivan <whitestranger7@gmail.com>
2023-07-27 11:19:53 +0200

    add: game functionality

--- nodes/memory_tile/memory_tile.gd
+++ nodes/memory_tile/memory_tile.gd
@@ -2,10 +2,42 @@ extends TextureButton
 class_name MemoryTile
 
 
+@onready var frame_image: TextureRect = $FrameImage
+@onready var image: TextureRect = $Image
+
+
+var _visible: bool = false
+var _index: int
 var _image_name: String
 
 
-func init(image_data: Dictionary) -> void:
+func init(index: int, image_data: Dictionary) -> void:
 	var image: TextureRect = $Image
 	image.texture = image_data.item_texture
 	_image_name = image_data.item_name
+	_index = index
+
+
+func set_visibility(value: bool) -> void:
+	frame_image.set_visible(value)
+	image.set_visible(value)
+	_visible = value
+
+
+func reveal_tile() -> void:
+	set_visibility(true)
+
+
+func hide_tile() -> void:
+	set_visibility(false)
+
+
+func can_be_selected() -> bool:
+	if _index is int and _visible == false and !GameManager._grid_disabled:
+		return true
+	return false
+
+
+func _on_pressed() -> void:
+	if can_be_selected():
+		GameManager.select_tile(self)
--- singletons/game_manager.gd
+++ singletons/game_manager.gd
@@ -1,6 +1,8 @@
 extends Node
 
 signal level_selected
+signal tile_selected
+signal game_won
 
 
 const LEVELS = {
@@ -13,21 +15,31 @@ const LEVELS = {
 }
 
 
+var _grid_disabled: bool = false
 var _selected_level: Dictionary
 var _images: Array[Dictionary]
 var _attempts: int
+var _selected_tiles: Array[MemoryTile]
+var _revealed_tiles: Array[int]
+var timer: Timer
 
 
 func setup_level(level: int) -> void:
 	_selected_level = LEVELS[level]
 	_attempts = 0
 	_images = []
+	timer = Timer.new()
 	while _images.size() < _selected_level.rows * _selected_level.cols:
 		var image = ImageManager.get_random_image()
 		if image not in _images:
 			_images.append(image)
 			_images.append(image)
 	_images.shuffle()
+
+
+func check_if_won() -> void:
+	if _revealed_tiles.size() >= _images.size():
+		game_won.emit()
 
 
 func select_level(level: int) -> void:
@@ -41,6 +53,36 @@ func select_level(level: int) -> void:
 
 
 func game_exit() -> void:
+	_images = []
+	_revealed_tiles = []
+	_attempts = 0
+	for scene in get_tree().get_nodes_in_group("won_scene"):
+		scene.queue_free()
 	for mt in get_tree().get_nodes_in_group("memory_tile"):
 		mt.queue_free()
 	NavigationManager.navigate(NavigationManager.Routes.MENU)
+
+
+func hide_selected_tiles() -> void:
+	for tl in _selected_tiles:
+		tl.hide_tile()
+	_selected_tiles = []
+	_grid_disabled = false
+
+
+func select_tile(tile: MemoryTile) -> void:
+	_selected_tiles.append(tile)
+	tile.reveal_tile()
+	if _selected_tiles.size() >= 2:
+		if _attempts <= 0:
+			_attempts += 1
+		if _selected_tiles[0]._image_name == _selected_tiles[1]._image_name:
+			for tl in _selected_tiles:
+				_revealed_tiles.append(tl._index)
+			_selected_tiles = []
+		else:
+			_attempts += 1
+			_grid_disabled = true
+			pass
+	tile_selected.emit()
+	check_if_won()
